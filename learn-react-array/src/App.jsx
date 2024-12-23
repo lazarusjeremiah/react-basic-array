6f6f64be80fea286da35e45d076502d8773a37df
lazarusjeremiah <lazarusjeremiah37@gmail.com>
2024-12-23 04:17:09 +0100

    Learn react arrays

---
+++ learn-react-array/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  const ninjaTurties = [
+    <h2>Donatello</h2>,
+    <h2>Michaelangelo</h2>,
+    <h2>Rafael</h2>,
+    <h2>Leonardo</h2>
+  ]
+
+  return (
+    <>
+      {ninjaTurties}
+    </>
+  )
+}
+
+export default App
